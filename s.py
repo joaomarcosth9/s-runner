ffb020abdea4733c87b5c567199de2926bb5057a
João Oliveira <joaomarcosnet10@gmail.com>
2022-08-04 10:50:00 -0300

    changed cpp compilation flags

--- s.py
+++ s.py
@@ -28,7 +28,7 @@ def run_interpreted(file, interpreter, infile):
         os.system(interpreter + file)
 
 if('.cpp' in filename):
-    command = 'g++ -Wall -lm --std=c++17 '
+    command = 'g++ -Wall -lm --std=c++17 -O2 '
     name = filename.split('/')[-1][:-4]
     os.system(command + filename + ' -o /tmp/' + name)
     if(run):
