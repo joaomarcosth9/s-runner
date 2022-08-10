a626595b016bb09e8cb4cc5fa96a7ba1d5952770
João Oliveira <joaomarcosnet10@gmail.com>
2022-08-10 02:34:59 -0300

    updating

--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,33 @@
-import os
+from os import system
 
 def placeholder_for_testcases():
     print("------------------")
 
 def compile(command_to_compile, full_path_to_file, destination_directory, short_name):
-    os.system(command_to_compile + full_path_to_file + ' -o ' + destination_directory + short_name)
+    system(command_to_compile + full_path_to_file + ' -o ' + destination_directory + short_name)
 
 def run_compiled(file_name, directory, inputs_list):
     if inputs_list:
         if len(inputs_list) == 1:
-            os.system(directory + file_name + ' < ' + directory + inputs_list[0])
+            system(directory + file_name + ' < ' + directory + inputs_list[0])
         else:
             placeholder_for_testcases()
             for infile in inputs_list:
                 print(f"# Input {inputs_list.index(infile) + 1}")
-                os.system(directory + file_name + ' < ' + directory + infile)
+                system(directory + file_name + ' < ' + directory + infile)
                 placeholder_for_testcases()
     else:
-        os.system(directory + file_name)
+        system(directory + file_name)
 
 def run_interpreted(interpreter, full_path_to_file, input_directory, inputs_list):
     if inputs_list:
         if len(inputs_list) == 1:
-            os.system(interpreter + full_path_to_file + ' < ' + input_directory + inputs_list[0])
+            system(interpreter + full_path_to_file + ' < ' + input_directory + inputs_list[0])
         else:
             placeholder_for_testcases()
             for infile in inputs_list:
                 print(f"# Input {inputs_list.index(infile) + 1}")
-                os.system(interpreter + full_path_to_file + ' < ' + input_directory + infile)
+                system(interpreter + full_path_to_file + ' < ' + input_directory + infile)
                 placeholder_for_testcases()
     else:
-        os.system(interpreter + full_path_to_file)
+        system(interpreter + full_path_to_file)
